62aa41d68b5eb8ebb23a189f26503c78dad2a690
Juan David Mendez <juandamendez21@gmail.com>
2024-08-17 19:21:16 -0500

    Header funcionando y se procede a crear la seccion de llamado de datos con un api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { CelularesComponent } from "./celulares/celulares.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CelularesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Phone-Finder';
 }
+
